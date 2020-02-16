78c745feda5272e4dd91add05e32f134d9bc8f7f
Zahra seyedi <za.seyyedi@gmail.com>
2020-02-16 15:44:39 +0330

    show poem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,138 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import { getPoem } from './fakePoemServic'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      poemList: []
+    };
+  }
+  UNSAFE_componentWillMount() {
+    //why unsafe?
+
+    this.setState({ poemList: getPoem() })
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <this.Border
+          title={this.state.poemList.title}
+
+          test={
+            this.state.poemList.hemistichs
+            // ["می‌خواهمت چنان که شب خسته خواب را",
+            //   "می‌جویمت چنان‌که لب تشنه آب را",
+            //   "محو توام چنان‌که ستاره به چشم صبح",
+            //   "یا شبنم سپیده‌دمان آفتاب را",
+            //   "بی‌تابم آن‌چنان که درختان برای باد",
+            //   "یا کودکان خفته به گهواره، خواب را",
+            //   "ای خواهشی که خواستنی‌تر ز پاسخی",
+            //   "با چون تو پرسشی چه نیازی جواب را"]
+          }
+          icon={["icon", "icon", "icon", "icon", "icon"]} />
+
+      </div>
+    );
+  }
+  Border = (props) => {
+    return (
+      <div className="border">
+        <this.Header title={props.title} />
+        <this.Top />
+        <this.Body test={props.test} />
+        <this.Footer icon={props.icon} />
+
+      </div>
+
+    )
+  }
+  Header = ({ title }) => {
+    return (
+      <div className="header">
+        <div className="circle"></div>
+        <div className="title">{title}</div>
+        <div className="dots">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
+      </div>
+    )
+
+  }
+  Top = () => {
+    return (
+      <div className="top">
+        <div className="poetContent">
+          <this.PoetB poetB={this.state.poemList.poets[0].name} />
+          <this.BookB bookB={this.state.poemList.book.name} />
+        </div>
+        <div className="icon"></div>
+      </div>
+    )
+  }
+  PoetB = ({ poetB }) => {
+    return (
+      <div className="poetB">{poetB}</div>
+    )
+  }
+  BookB = ({ bookB }) => {
+    return (
+      <div className="bookB">{bookB}</div>
+    )
+  }
+  Body = ({ test }) => {
+    let temp = [];
+    for (let i = 0; i < test.length; i += 1) {
+      temp.push(test[i]);
+    }
+    console.log(temp);
+
+    // if(test.length % 2 == 1){
+    //   temp.push([test[test.length-1,""]])
+    // }
+    return (
+      <div className="body">
+        <div className="poems">
+          {
+            temp.map(item => {
+              return (
+                <div>
+                  <div>{item.text[0]}</div>
+                  <div>{item.text[1]}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="icons">
+          <div className="icosContent"></div>
+          <div className="icosContent2"></div>
+        </div>
+      </div>
+    )
+  }
+  Footer = ({ icon }) => {
+
+    return (
+      <div className="footer">
+        {
+          icon.map(item => {
+            return (
+              <div className={item}></div>
+            )
+          })
+        }
+        {/* <div className ="icon"></div>
+          <div className ="icon"></div>
+          <div className ="icon"></div>
+          <div className ="icon"></div>
+          <div className ="icon"></div> */}
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/fakePoemServic.js
@@ -0,0 +1,66 @@
+export function getPoem(){
+    return {
+    "suggestedTags": [],
+    "title": "بخش ۹",
+    "_id": "5cffd9b7e38eaf7a54c59c43",
+    "book": {
+        "_id": "5cffd9a7e38eaf7a54c5209f",
+        "name": "شاهنامه"
+    },
+    "poets": [
+        {
+            "_id": "5cffd9a7e38eaf7a54c5209d",
+            "name": "فردوسی"
+        }
+    ],
+    "poetId": "5cffd9a7e38eaf7a54c5209d",
+    "hemistichs": [
+        {
+            "isHidden": false,
+            "_id": "5cffd9b7e38eaf7a54c59c44",
+            "text": [
+                "چو شب روز شد بامداد پگاه",
+                "بفرمود تا بازگردد سپاه"
+            ]
+        },
+        {
+            "isHidden": false,
+            "_id": "5cffd9b7e38eaf7a54c59c45",
+            "text": [
+                "بیامد دو رخساره همرنگ نی",
+                "چو شب تیره گشت اندر آمد بری"
+            ]
+        },
+        {
+            "isHidden": false,
+            "_id": "5cffd9b7e38eaf7a54c59c46",
+            "text": [
+                "یکی نامه بنوشت نزد پسر",
+                "که کژی به باغ اندر آورد بر"
+            ]
+        },
+        {
+            "isHidden": false,
+            "_id": "5cffd9b7e38eaf7a54c59c47",
+            "text": [
+                "چنان شد ز بالین ما اردشیر",
+                "کزان سان نجست از کمان ایچ تیر"
+            ]
+        },
+        {
+            "isHidden": false,
+            "_id": "5cffd9b7e38eaf7a54c59c48",
+            "text": [
+                "سوی پارس آمد بجویش نهان",
+                "مگوی این سخن با کسی در جهان"
+            ]
+        }
+    ],
+    "views": {
+        "totalViews": 25
+    },
+    "modifiedDate": "2019-06-11T16:41:27.511Z",
+    "imageId": "5cf3e496727c2d2db0edaa83",
+    "note": ""
+}
+}
